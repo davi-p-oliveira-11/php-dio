7597ab382f65a9e5b1e754a54cd96df0ba7c3d0d
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-28 10:31:31 -0300

    added conditional if-else

---
+++ curso02/condicional-if-else.php
@@ -0,0 +1,15 @@
+<?php
+
+  $nota = 8;
+
+  if ($nota >= 7) {
+    echo "Aluno(a) aprovado !";
+  } else {
+    echo "Aluno(a) reprovado ";
+  }
+?>
+
+<?php
+  
+ echo $nota >= 7 ? "Aluno(a) aprovado !"  : "Aluno(a) reprovado !"
+?>
